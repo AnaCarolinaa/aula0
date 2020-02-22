1216a282e6589076f9e97b0af7687b3707ba8766
Ana Carolina da Silva - 817117218 <817117218@animaedu.intranet>
2020-02-21 21:50:35 -0300

    versão inicial

---
+++ Aluno.java
@@ -0,0 +1,21 @@
+public class Aluno
+{
+   private String nome;
+   private int ID;
+   
+   public Aluno(String nome, int ID)
+   {
+      this.nome = nome;
+      this.ID = ID;
+   }
+   
+   public String getNome()
+   {
+      return this.nome;
+   }
+   
+   public int getID()
+   {
+      return this.ID;
+   }
+}
